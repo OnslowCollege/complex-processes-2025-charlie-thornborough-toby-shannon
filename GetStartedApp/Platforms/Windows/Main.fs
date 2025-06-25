09a734b0624079e06c53e1de80dffc3a6de9ebaa
Chupps1812 <107961398+Chupps1812@users.noreply.github.com>
2025-06-25 14:40:42 +1200

    Adding in all the project files for the F# testy project, and for VS to work

---
+++ GetStartedApp/Platforms/Windows/Main.fs
@@ -0,0 +1,9 @@
+﻿namespace GetStartedApp.WinUI
+
+open System
+
+module Program =
+    [<EntryPoint; STAThread>]
+    let main args =
+        do FSharp.Maui.WinUICompat.Program.Main(args, typeof<GetStartedApp.WinUI.App>)
+        0
