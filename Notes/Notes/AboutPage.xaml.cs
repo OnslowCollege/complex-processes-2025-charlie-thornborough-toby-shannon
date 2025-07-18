516de1c32b7041f1ae489d623356a82fd7cadca9
Chupps1812 <107961398+Chupps1812@users.noreply.github.com>
2025-07-18 14:25:47 +1200

    Just working through the task, might not be able to finish cause the images no longer exist...

--- Notes/Notes/AboutPage.xaml.cs
+++ Notes/Notes/AboutPage.xaml.cs
@@ -2,7 +2,12 @@ namespace Notes;
 
 public partial class AboutPage : ResourceDictionary
 {
-	public AboutPage()
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+    public AboutPage()
 	{
 		InitializeComponent();
 	}
